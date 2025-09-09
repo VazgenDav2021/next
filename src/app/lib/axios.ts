e6770b2655f1d9ca8fab1d8b897be792dc0a29f9
vazgengrid <vdavtyan@griddynamics.com>
2025-09-09 10:02:03 +0400

    push

--- src/app/lib/axios.ts
+++ src/app/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_STRAPI_URL || "https://steadfast-renewal-d75371361d.strapiapp.com/api/api",
+  baseURL: process.env.NEXT_PUBLIC_STRAPI_URL || "https://steadfast-renewal-d75371361d.strapiapp.com/api",
   headers: {
     "Content-Type": "application/json",
   },
